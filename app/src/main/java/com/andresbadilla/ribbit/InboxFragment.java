c4caeea78304dd41e032ec3cfa0376ea995671a1
Andres Badilla <andres_badilla@mckinsey.com>
2015-09-24 19:15:54 -0600

    -Friends module complete (Add and delete)

---
+++ app/src/main/java/com/andresbadilla/ribbit/InboxFragment.java
@@ -0,0 +1,41 @@
+package com.andresbadilla.ribbit;
+
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Andres Badilla on 9/23/2015.
+ */
+public class InboxFragment extends ListFragment {
+
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+   // private static final String ARG_SECTION_NUMBER = "section_number";
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static InboxFragment newInstance(int sectionNumber) {
+        InboxFragment fragment = new InboxFragment();
+        //Bundle args = new Bundle();
+        //args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        //fragment.setArguments(args);
+        return fragment;
+    }
+
+    public InboxFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_inbox, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/andresbadilla/ribbit/ParseConstants.java
@@ -0,0 +1,12 @@
+package com.andresbadilla.ribbit;
+
+/**
+ * Created by Andres Badilla on 9/23/2015.
+ */
+public final class ParseConstants {
+    //Class names
+
+    //Field names
+    public static final String KEY_USERNAME ="username";
+    public static final String KEY_FRIENDS_RELATION="friendsRelation";
+}
