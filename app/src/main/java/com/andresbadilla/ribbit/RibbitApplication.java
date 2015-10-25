3abe2bd294ca36265d2162035f08ae304900f17a
Andres Badilla <andres_badilla@mckinsey.com>
2015-10-25 14:29:46 -0600

    - Final product with push notifications (not fully notifications working)

--- app/src/main/java/com/andresbadilla/ribbit/RibbitApplication.java
+++ app/src/main/java/com/andresbadilla/ribbit/RibbitApplication.java
@@ -3,6 +3,7 @@ package com.andresbadilla.ribbit;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseInstallation;
 
 public class RibbitApplication extends Application {
 
@@ -12,5 +13,8 @@ public class RibbitApplication extends Application {
         // Enable Local Datastore.
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "77V26k41V0IC1FoD2kU92WosPycN7Dc6E2NfcfFd", "oyhOl5ATMzsL8D14v41GHOwqfyw5AjevoQ6OuoNO");
+
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+
     }
 }
